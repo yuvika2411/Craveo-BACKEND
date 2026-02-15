d7e5f92648a005937011f69140f47d965fd44349
Yuvika Jindal <yuvikajindal241106@gmail.com>
2026-02-15 18:05:07 +0530

    Restaurant api

--- src/main/java/com/Craveo/service/RestaurantServiceImp.java
+++ src/main/java/com/Craveo/service/RestaurantServiceImp.java
@@ -3,11 +3,14 @@ package com.Craveo.service;
 import com.Craveo.Repository.AddressRepository;
 import com.Craveo.Repository.RestaurantRepository;
 import com.Craveo.dto.RestaurantDto;
+import com.Craveo.model.Address;
 import com.Craveo.model.Restaurant;
 import com.Craveo.model.User;
 import com.Craveo.request.CreateRestaurantRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.core.Local;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class RestaurantServiceImp implements RestaurantService{
@@ -23,13 +26,36 @@ public class RestaurantServiceImp implements RestaurantService{
 
     @Override
     public Restaurant createRestaurant(CreateRestaurantRequest req, User user) {
-        //hi
-        return null;
+        Address address= addressRepository.save(req.getAddress());
+        Restaurant restaurant= new Restaurant();
+
+        restaurant.setAddress(address);
+        restaurant.setContactInformation(req.getContactInformation());
+        restaurant.setCuisineType(req.getCuisineType());
+        restaurant.setDescription(req.getDescription());
+        restaurant.setImages(req.getImages());
+        restaurant.setName(req.getName());
+        restaurant.setOpeningHrs(req.getOpeningHrs());
+        restaurant.setRegistrationDate(LocalDateTime.now());
+        restaurant.setOwner(user);
+
+        return restaurantRepository.save(restaurant);
     }
 
     @Override
     public Restaurant updateRestaurant(Long restaurandId, CreateRestaurantRequest updateRestaurant) throws Exception {
-        return null;
+        Restaurant restaurant= findRestaurantById(restaurandId);
+
+        if(restaurant.getCuisineType()!=null){
+            restaurant.setCuisineType(updateRestaurant.getCuisineType());
+        }
+        if(restaurant.getDescription()!=null){
+            restaurant.setDescription(updateRestaurant.getDescription());
+        }
+        if(restaurant.getName()!=null){
+            restaurant.setName(updateRestaurant.getName());
+        }
+        return restaurantRepository.save(restaurant);
     }
 
     @Override
