600f34422c157039c665e47bc8ee67862b5b5c2f
Yuvika Jindal <yuvikajindal241106@gmail.com>
2026-04-29 22:47:24 +0530

    foodstate

--- frontend/src/component/State/store.js
+++ frontend/src/component/State/store.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import { authReducer } from "./Authentication/Reducer";
 import { configureStore } from "@reduxjs/toolkit";
+import restaurantReducer from "./Restaurant/Reducer";
 
 const rootReducer = combineReducers({
-    auth: authReducer
+    auth: authReducer,
+    restaurant:restaurantReducer
 })
 
 export const store = configureStore({
