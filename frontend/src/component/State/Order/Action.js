cf2ba4e410490805eb56ac9f995247545d1ee4bc
Yuvika Jindal <yuvikajindal241106@gmail.com>
2026-04-30 22:01:26 +0530

    states

--- frontend/src/component/State/Order/Action.js
+++ frontend/src/component/State/Order/Action.js
@@ -1,15 +1,47 @@
-export const CREATE_ORDER_REQUEST = "CREATE_ORDER_REQUEST";
-export const CREATE_ORDER_SUCCESS = "CREATE_ORDER_SUCCESS";
-export const CREATE_ORDER_FAILURE = "CREATE_ORDER_FAILURE";
+import {api} from "../../../config/api";
+import {
+    CREATE_ORDER_REQUEST,
+    CREATE_ORDER_SUCCESS,
+    CREATE_ORDER_FAILURE,
+    GET_USERS_ORDERS_REQUEST,
+    GET_USERS_ORDERS_SUCCESS,
+    GET_USERS_ORDERS_FAILURE,
+} from "./ActionType";
 
-export const GET_USERS_ORDERS_REQUEST = "GET_USERS_ORDERS_REQUEST";
-export const GET_USERS_ORDERS_SUCCESS = "GET_USERS_ORDERS_SUCCESS";
-export const GET_USERS_ORDERS_FAILURE = "GET_USERS_ORDERS_FAILURE";
+export const createOrder = (reqData) => {
+    return async (dispatch) => {
+        dispatch({ type: CREATE_ORDER_REQUEST });
+        try {
+            const { data } = await api.post("/api/order", reqData.order, {
+                headers: {
+                    Authorization: `Bearer ${reqData.jwt}`,
+                },
+            });
+            // if (data.payment_url) {
+            //     window.location.href = data.payment_url;
+            // }
+            console.log("created order data", data);
+            dispatch({ type: CREATE_ORDER_SUCCESS, payload: data });
+        } catch (error) {
+            console.log("error ", error);
+            dispatch({ type: CREATE_ORDER_FAILURE, payload: error });
+        }
+    };
+};
 
-export const GET_USER_NOTIFICATION_REQUEST="GET_USER_NOTIFICATION_REQUEST";
-export const GET_USER_NOTIFICATION_SUCCESS="GET_USER_NOTIFICATION_SUCCESS";
-export const GET_USER_NOTIFICATION_FAILURE="GET_USER_NOTIFICATION_FAILURE";
-
-export const UPDATE_ORDER_STATUS_REQUEST="UPDATE_ORDER_STATUS_REQUEST";
-export const UPDATE_ORDER_STATUS_SUCCESS="UPDATE_ORDER_STATUS_SUCCESS";
-export const UPDATE_ORDER_STATUS_FAILURE="UPDATE_ORDER_STATUS_FAILURE";
+export const getUsersOrders = (jwt) => {
+    return async (dispatch) => {
+        dispatch({ type: GET_USERS_ORDERS_REQUEST });
+        try {
+            const { data } = await api.get(`/api/order/user`, {
+                headers: {
+                    Authorization: `Bearer ${jwt}`,
+                },
+            });
+            console.log("users order ", data);
+            dispatch({ type: GET_USERS_ORDERS_SUCCESS, payload: data });
+        } catch (error) {
+            dispatch({ type: GET_USERS_ORDERS_FAILURE, payload: error });
+        }
+    };
+};
