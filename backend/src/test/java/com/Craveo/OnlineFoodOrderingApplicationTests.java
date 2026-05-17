84be489720ffe756273cba20a91b928d9a01fe6b
Yuvika Jindal <yuvikajindal241106@gmail.com>
2026-05-17 10:33:52 +0530

    Update OnlineFoodOrderingApplicationTests.java

--- backend/src/test/java/com/Craveo/OnlineFoodOrderingApplicationTests.java
+++ backend/src/test/java/com/Craveo/OnlineFoodOrderingApplicationTests.java
@@ -3,6 +3,7 @@ package com.Craveo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+//main file
 @SpringBootTest
 class OnlineFoodOrderingApplicationTests {
 
