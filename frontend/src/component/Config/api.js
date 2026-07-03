8f19a5f8ae64ec93848d72352a4c1310cfebc3f8
yuvika2411 <yuvikajindal241106@gmail.com>
2026-07-03 17:59:47 +0530

    create res issue

--- frontend/src/component/Config/api.js
+++ frontend/src/component/Config/api.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
-    headers: {
-        "Content-Type": "application/json",
-    },
+    baseURL: import.meta.env.VITE_API_URL
 });
 
 // AUTO TOKEN ATTACH
